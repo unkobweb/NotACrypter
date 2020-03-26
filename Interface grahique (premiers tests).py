cb13d9eba6d0aa659b19c6e17dc98af05fb33fe4
unknown <lucas.leger32@gmail.com>
2020-03-26 21:00:27 +0100

    radiobuttons + hash button integration

--- Interface grahique (premiers tests).py
+++ Interface grahique (premiers tests).py
@@ -7,13 +7,13 @@ window = Tk()
 
 # personnamiser cette fenêtre
 window.title("NotACrypter")
-window.geometry("1080x720")
+window.geometry("1370x720")
 window.minsize(800, 450)
 window.config(background='#555555')
 
 # premier texte
 label_title = Label(window, text="Fonction de hashage", font=(
-    "Courrier", 14), bg='#555555', fg='white', height='10').place(x=5, y=-95)
+    "Courrier", 14), bg='#555555', fg='white', height='10').place(x=70, y=75)
 # label_title.pack()
 
 label_title2 = Label(window, text="Votre document", font=(
@@ -30,23 +30,7 @@ def mfileopen():
 button = Button(text="Sélectionner un document", width=30,
                 command=mfileopen).place(x=300, y=377)
 
-var = IntVar()
-
-check = Checkbutton(window, text="SHA-1", variable=var,
-                    font=("Courrier", 10), bg='#555555', fg='white')
-check.place(x=275, y=175)
-check2 = Checkbutton(window, text="SHA-256", variable=var,
-                     font=("Courrier", 10), bg='#555555', fg='white')
-check2.place(x=375, y=175)
-check3 = Checkbutton(window, text="SHA-512", variable=var,
-                     font=("Courrier", 10), bg='#555555', fg='white')
-check3.place(x=475, y=175)
-check4 = Checkbutton(window, text="MD5", variable=var,
-                     font=("Courrier", 10), bg='#555555', fg='white')
-check4.place(x=575, y=175)
-check5 = Checkbutton(window, text="Blake2B", variable=var,
-                     font=("Courrier", 10), bg='#555555', fg='white')
-check5.place(x=675, y=175)
+hashing_button = Button(text="Hasher", width=50).place(x=110,y=525)
 
 vals = ['sha1', 'sha256', 'sha512', 'md5', 'blake2b']
 etiqs = ['SHA-1', 'SHA-256', 'SHA-512', 'MD5', 'Blake2B']
@@ -56,7 +40,7 @@ for i in range(5):
     b = Radiobutton(window, variable=varGr,
                     text=etiqs[i], value=vals[i], activebackground="#555555", bg='#555555', activeforeground="white", foreground="white", selectcolor="black")
     b.pack(side='left', expand=1)
-    b.place(x=5, y=(20*i)+30)
+    b.place(x=(100*i)+300, y=175)
 
 # afficher
 window.mainloop()
